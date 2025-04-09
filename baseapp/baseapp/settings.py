be760520787e290e156da3f10b50729b97c8fc7b
Rayhan Khan <rayhankhan1226@gmail.com>
2025-04-09 17:15:30 +0600

    initial changes in setting.py

--- baseapp/baseapp/settings.py
+++ baseapp/baseapp/settings.py
@@ -212,5 +212,4 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
-# EMAIL_HOST_USER = 'clay.world.max.1011@gmail.com'
-# EMAIL_HOST_PASSWORD = 'usre qyzt nxyz zbsv'
+
