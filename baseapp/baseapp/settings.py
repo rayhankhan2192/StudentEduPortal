7dc0ec47ed496d2ec68ca80d7bfa8f8ca73bcb64
Rayhan Khan <rayhankhan1226@gmail.com>
2025-04-09 17:14:08 +0600

    initial changes in setting.py

--- baseapp/baseapp/settings.py
+++ baseapp/baseapp/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 from datetime import timedelta
+import os
+from dotenv import load_dotenv
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -198,9 +201,16 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 # Email services
+# BASE_DIR = Path(__file__).resolve().parent.parent
+# load_dotenv(dotenv_path=BASE_DIR / '.env')
+load_dotenv() 
+# Use the variables
+
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'clay.world.max.1011@gmail.com'
-EMAIL_HOST_PASSWORD = 'usre qyzt nxyz zbsv'
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+# EMAIL_HOST_USER = 'clay.world.max.1011@gmail.com'
+# EMAIL_HOST_PASSWORD = 'usre qyzt nxyz zbsv'
